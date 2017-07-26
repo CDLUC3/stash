6fb7116c3a4dee73753084a9d84d11dc9e9daf19
David Moles <david.moles@ucop.edu>
2017-07-26 16:59:15 -0700

    Get unit test load path working

--- stash_datacite/spec/spec_helper.rb
+++ stash_datacite/spec/spec_helper.rb
@@ -1,7 +1,6 @@
 # ------------------------------------------------------------
 # Simplecov
 
-# TODO: use `SimpleCov.start 'rails'` to check *full* coverage
 require 'simplecov' if ENV['COVERAGE']
 
 # ------------------------------------------------------------
@@ -22,4 +21,13 @@ ENV['STASH_ENV'] = 'test'
 require 'stash_engine'
 require 'stash_datacite'
 
-puts $LOAD_PATH
+class ApplicationController < ActionController::Base
+  # hack to get around the fact we're not running in an app
+end
+
+%w(stash_engine stash_datacite stash_discovery).map do |engine_name|
+  engine_path = Gem::Specification.find_by_name(engine_name).gem_dir
+  models_path = "#{engine_path}/app/models/#{engine_name}"
+  $LOAD_PATH.unshift(models_path) if File.directory?(models_path)
+  Dir.glob("#{models_path}/**/*.rb").sort.each(&method(:require))
+end
