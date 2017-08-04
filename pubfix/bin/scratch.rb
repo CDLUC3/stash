31c9d1c6e6b2a2d1d104457c0b0360b84c12458a
David Moles <david.moles@ucop.edu>
2017-08-04 10:17:26 -0700

    Specify mysql2 0.4+

--- pubfix/bin/scratch.rb
+++ pubfix/bin/scratch.rb
@@ -2,17 +2,9 @@
 
 Dir.glob(File.expand_path('../../lib/*.rb', __FILE__)).sort.each(&method(:require))
 
-include Publishers
+bad_to_good = Publishers::BAD_PUBLISHER_TENANT_IDS.keys.map do |bad|
+  good = Publishers.good_publisher_for(bad)
+  "'#{bad}' => '#{good}'"
+end
 
-require 'rsolr'
-
-solr = RSolr.connect(url: 'http://uc3-dash2solr-prd.cdlib.org:8983/solr/geoblacklight')
-response = solr.get('select', params: { q: '*:*', fl: 'uuid,dc_publisher_s', rows: 500 })
-response_body = response['response']
-docs = response_body['docs']
-publishers_by_doi = docs.map { |doc| [doc['uuid'], doc['dc_publisher_s']] }.to_h
-to_fix = publishers_by_doi.reject { |_, pub| CURRENT.include?(pub) }
-
-# to_fix.each { |k, v| puts "#{k}\t#{v}" }
-
-puts to_fix.keys.map { |doi| "           '#{doi.sub(/^doi:/, '')}'" }.join(",\n")
+puts bad_to_good.join(",\n")
