74b4bfe89e81bfde66cc725b513ae15cb29c4b97
Scott Fisher <scott.fisher@ucop.edu>
2019-07-30 11:52:26 -0700

    make travis work?

--- stash_engine/app/serializers/stash_engine/share_serializer.rb
+++ stash_engine/app/serializers/stash_engine/share_serializer.rb
@@ -2,7 +2,9 @@ module StashEngine
   class ShareSerializer
     include SerializerMixin
 
-    REJECT_COLUMNS = %w[id resource_id].freeze
+    # I don't know why sharing link (a method, not a database field) would be included, but Travis is trying
+    # to use it as output!
+    REJECT_COLUMNS = %w[id resource_id sharing_link].freeze
     COLUMNS = (StashEngine::Share.column_names - REJECT_COLUMNS).freeze
 
   end
