3d95123ce80822d429d947ad8dff1abc54865a50
Scott Fisher <scott.fisher@ucop.edu>
2019-07-30 12:10:48 -0700

    F-Travis.

--- stash_engine/app/serializers/stash_engine/share_serializer.rb
+++ stash_engine/app/serializers/stash_engine/share_serializer.rb
@@ -3,8 +3,8 @@ module StashEngine
     include SerializerMixin
 
     # I don't know why sharing link (a method, not a database field) would be included, but Travis is trying
-    # to use it as output!
-    REJECT_COLUMNS = %w[id resource_id sharing_link].freeze
+    # to use wrong columns from database as output.  WTF?  This is for you, Travis.
+    REJECT_COLUMNS = %w[id resource_id sharing_link expiration_date].freeze
     COLUMNS = (StashEngine::Share.column_names - REJECT_COLUMNS).freeze
 
   end
